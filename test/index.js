06dbeeab844fafb59c7173d45200f5e794be9a96
Jack Boberg <jack.boberg@gmail.com>
2016-05-17 10:06:04 -0400

    Setup basic test runner and coverage tools

---
+++ test/index.js
@@ -0,0 +1,17 @@
+const Code = require('code');
+const Lab = require('lab');
+
+const RabbitRPC = require('..');
+
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('RabbitRPC', function () {
+  it('exports a function', function (done) {
+    expect(RabbitRPC).to.be.a.function();
+    done();
+  });
+});

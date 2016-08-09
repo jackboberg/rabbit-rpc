053d3424cc8a85ccb8f8f9046db5e7410e7060c9
Jack Boberg <jack.boberg@gmail.com>
2016-08-09 17:01:03 -0400

    test: Create integration test suite

---
+++ test/client.js
@@ -0,0 +1,78 @@
+const Code = require('code');
+const Lab = require('lab');
+const Sinon = require('sinon');
+
+const RabbitRPC = require('..');
+
+var lab = exports.lab = Lab.script();
+
+var afterEach = lab.afterEach;
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+// eslint-disable-next-line no-process-env
+const AMQP_HREF = process.env.AMQP_HREF || 'amqp://user:password@localhost';
+const QUEUE = 'rpc-queue-client-test';
+
+var rabbit, server;
+
+before(function (done) {
+  server = Sinon.stub();
+  rabbit = RabbitRPC(AMQP_HREF);
+
+  rabbit.server(QUEUE, server);
+  done();
+});
+
+describe('RabbitRPC.client', function () {
+  var msg, result;
+
+  beforeEach(function (done) {
+    msg = { testMessage: true };
+    result = { testResult: true };
+    server.yields(null, result);
+    done();
+  });
+
+  afterEach(function (done) {
+    server.reset();
+    done();
+  });
+
+  it('exports a function', function (done) {
+    expect(rabbit.client).to.be.a.function();
+    done();
+  });
+
+  describe('when a server yields a result', function () {
+    it('yields result', function (done) {
+      rabbit.client(QUEUE, msg, function (err, res) {
+        expect(err).to.not.exist();
+        expect(res).to.deep.equal(result);
+        done();
+      });
+    });
+  });
+
+  describe('when a server yields an error', function () {
+    var error;
+
+    beforeEach(function (done) {
+      error = new Error('RPC.server Error');
+      server.yields(error);
+      done();
+    });
+
+    it('yields an error', function (done) {
+      rabbit.client(QUEUE, msg, function (err, res) {
+        expect(err).to.exist();
+        expect(err.message).to.equal(error.message);
+        expect(res).to.not.exist();
+        done();
+      });
+    });
+  });
+});
---
+++ test/server.js
@@ -0,0 +1,56 @@
+const Code = require('code');
+const Lab = require('lab');
+const Sinon = require('sinon');
+const Util = require('util');
+
+const RabbitRPC = require('..');
+
+var lab = exports.lab = Lab.script();
+
+var before = lab.before;
+var beforeEach = lab.beforeEach;
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+// eslint-disable-next-line no-process-env
+const AMQP_HREF = process.env.AMQP_HREF || 'amqp://user:password@localhost';
+const QUEUE = 'rpc-queue-server-test';
+
+var rabbit, queue;
+
+before(function (done) {
+  rabbit = RabbitRPC(AMQP_HREF);
+  done();
+});
+
+describe('RabbitRPC.server', function () {
+  it('exports a function', function (done) {
+    expect(rabbit.server).to.be.a.function();
+    done();
+  });
+
+  describe('when client sends a message', function () {
+    var message;
+
+    beforeEach(function (done) {
+      queue = Util.format('%s-message', QUEUE);
+      message = { testMessage: true };
+
+      rabbit.client(queue, message, Sinon.stub());
+      done();
+    });
+
+    it('recieves the message', function (done) {
+      rabbit.server(queue, function (msg, cb) {
+        expect(msg).to.deep.equal(message);
+        return cb(null, {}), done();
+      });
+    });
+  });
+
+  it('supports setting capacity', { skip: true }, function (done) {
+    expect(true).to.be.false();
+    done();
+  });
+});
